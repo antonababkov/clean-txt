9e0007b5f5ec6aea78bceca3c432caa6cae346a0
antonababkov <ababkov.ao@yandex.ru>
2026-07-28 17:51:55 +0300

    fix(frontend) удалить редиректы при переходе на админские маршруты при открытии ссылки в новой вкладке и перезагрузки страницы.
    Добавлен компонент-обрёртка AuthLoader.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "./store/hooks";
-import { fetchMe } from "./store/slices/authSlice";
+/* import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { fetchMe } from "./store/slices/authSlice"; */
 
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
@@ -12,77 +12,80 @@ import History from "./pages/History";
 import PrivateRoute from "./components/common/PrivateRoute";
 import Navbar from "./components/common/Navbar";
 import NotFound404 from "./pages/NotFound404";
+import AuthLoader from "./components/common/AuthLoader";
 
 import TasksList from "./components/admin/TasksList";
 import AdminRoute from "./components/admin/AdminRoute";
 import AdminPanel from "./pages/AdminPanel";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const accessToken = useAppSelector((state) => state.auth.accessToken);
+  /*   const dispatch = useAppDispatch();
+  const accessToken = useAppSelector((state) => state.auth.accessToken); */
 
   useEffect(() => {
     const theme = localStorage.getItem("theme") || "light";
     document.documentElement.className = theme;
   }, []);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     if (accessToken) {
       dispatch(fetchMe());
     }
-  }, [accessToken, dispatch]);
+  }, [accessToken, dispatch]); */
 
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className="container mx-auto mt-4">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/new"
-            element={
-              <PrivateRoute>
-                <NewTask />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/history"
-            element={
-              <PrivateRoute>
-                <History />
-              </PrivateRoute>
-            }
-          />
+      <AuthLoader>
+        <Navbar />
+        <div className="container mx-auto mt-4">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/new"
+              element={
+                <PrivateRoute>
+                  <NewTask />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/history"
+              element={
+                <PrivateRoute>
+                  <History />
+                </PrivateRoute>
+              }
+            />
 
-          <Route
-            path="/admin/tasks"
-            element={
-              <AdminRoute>
-                <TasksList />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <AdminRoute>
-                <AdminPanel />
-              </AdminRoute>
-            }
-          />
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
-      </div>
+            <Route
+              path="/admin/tasks"
+              element={
+                <AdminRoute>
+                  <TasksList />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <AdminRoute>
+                  <AdminPanel />
+                </AdminRoute>
+              }
+            />
+            <Route path="*" element={<NotFound404 />} />
+          </Routes>
+        </div>
+      </AuthLoader>
     </BrowserRouter>
   );
 }
