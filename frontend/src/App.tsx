85892c343ee8be1dabd83f3d91520be303dda3b8
antonababkov <ababkov.ao@yandex.ru>
2026-08-13 17:57:12 +0300

    refactor(App.tsx): добавить ленивую загрузку страниц

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useWebSocket } from "./hooks/useWebSocket";
 import { useAppSelector } from "./store/hooks";
 import { Toaster, toast } from "react-hot-toast";
-
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Dashboard from "./pages/Dashboard";
-import NewTask from "./pages/NewTask";
-import History from "./pages/History";
 
 import PrivateRoute from "./components/common/PrivateRoute";
 import Navbar from "./components/common/Navbar";
 import NotFound404 from "./pages/NotFound404";
 import AuthLoader from "./components/common/AuthLoader";
-
-import TasksList from "./components/admin/TasksList";
 import AdminRoute from "./components/admin/AdminRoute";
-import AdminPanel from "./pages/AdminPanel";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
+const Login = lazy(() => import("./components/auth/Login"));
+const Register = lazy(() => import("./components/auth/Register"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const NewTask = lazy(() => import("./pages/NewTask"));
+const History = lazy(() => import("./pages/History"));
+const AdminPanel = lazy(() => import("./pages/AdminPanel"));
+const TasksList = lazy(() => import("./components/admin/TasksList"));
 
 function App() {
   const token = useAppSelector((state) => state.auth.accessToken);
   const wsUrl = import.meta.env.VITE_WS_URL;
-
   const { messages } = useWebSocket(wsUrl, token);
 
   useEffect(() => {
@@ -46,52 +45,54 @@ function App() {
         <AuthLoader>
           <Navbar />
           <div className="container px-2 pb-4 mx-auto mt-4">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <Dashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/new"
-                element={
-                  <PrivateRoute>
-                    <NewTask />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/history"
-                element={
-                  <PrivateRoute>
-                    <History />
-                  </PrivateRoute>
-                }
-              />
+            <Suspense fallback={<LoadingSpinner />}>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  path="/"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/new"
+                  element={
+                    <PrivateRoute>
+                      <NewTask />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/history"
+                  element={
+                    <PrivateRoute>
+                      <History />
+                    </PrivateRoute>
+                  }
+                />
 
-              <Route
-                path="/admin/tasks"
-                element={
-                  <AdminRoute>
-                    <TasksList />
-                  </AdminRoute>
-                }
-              />
-              <Route
-                path="/admin"
-                element={
-                  <AdminRoute>
-                    <AdminPanel />
-                  </AdminRoute>
-                }
-              />
-              <Route path="*" element={<NotFound404 />} />
-            </Routes>
+                <Route
+                  path="/admin/tasks"
+                  element={
+                    <AdminRoute>
+                      <TasksList />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin"
+                  element={
+                    <AdminRoute>
+                      <AdminPanel />
+                    </AdminRoute>
+                  }
+                />
+                <Route path="*" element={<NotFound404 />} />
+              </Routes>
+            </Suspense>
           </div>
         </AuthLoader>
       </div>
